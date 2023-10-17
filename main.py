48419921cde9d3165e13e7d5b41f79f8aac33e15
SamRudgyardBath <sr2116@bath.ac.uk>
2023-10-17 14:07:33 +0100

    Plot 2 Stars

--- main.py
+++ main.py
@@ -14,15 +14,20 @@ from star import Star
 
 def Main():
     samples = 20
-    star1 = Star(0, 2, samples)
-    x, y, z = star1.nodes
+    star1 = Star(np.array([0,0,0]), 2, samples)
+    star2 = Star(np.array([10,0,0]), 5, samples)
+    
+    listStars = [star1, star2]
     ax = plt.figure().add_subplot(projection='3d')
-    ax.scatter(x, y, z)
-    ax.axis('equal')
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
-    ax.plot_surface(x, y, z, linewidth=0.2, antialiased=True)
+    for i in range(0, 2):
+        x, y, z = listStars[i].nodes
+        position = listStars[i].position
+        ax.scatter(x-position[0], y-position[1], z-position[2])
+        ax.axis('equal')
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+        ax.plot_surface(x-position[0], y-position[1], z-position[2], linewidth=0.2, antialiased=True)
     plt.show()
 
 if __name__ == "__main__":
