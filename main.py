e6167661e99c9bc39a08232bb891b1b9faa6c995
SamRudgyardBath <sr2116@bath.ac.uk>
2023-10-17 11:25:42 +0100

    Remove central origin point

--- main.py
+++ main.py
@@ -12,23 +12,25 @@ from geometric_functions import RotateY, RotateY
 from node import Node
 
 def Main():
-    samples = 1000
-    points = np.empty([3, samples])
+    samples = 100
+    points = np.empty([samples, 3])
     phi = np.pi * (np.sqrt(5.) - 1.)  # golden angle in radians
 
-    for i in range(samples):
+    for i in range(0,samples):
         y = 1 - (i / float(samples - 1)) * 2  # y goes from 1 to -1
         radius = np.sqrt(1 - y * y)  # radius at y
 
-        theta = phi * i  # golden angle increment
+        theta = phi * (i+1)  # golden angle increment
 
         x = np.cos(theta) * radius
         z = np.sin(theta) * radius
 
-        points[0, i], points[1, i], points[2, i] = x, y, z
+        points[i, 0], points[i, 1], points[i, 2] = x, y, z
     
-    plt.figure().add_subplot(111, projection='3d').scatter(points[0], points[1], points[2])
-    plt.axis('equal')
+    ax = plt.figure().add_subplot(projection='3d')
+    ax.scatter(points[:,0], points[:,1], points[:,2])
+    ax.axis('equal')
+    ax.plot_surface(points[:,0], points[:,1], points[:,2], linewidth=0.2, antialiased=True)
     plt.show()
 
 if __name__ == "__main__":
