a40765f0919daccd234c8bc1cb2ada18d16d82ea
SamRudgyardBath <sr2116@bath.ac.uk>
2023-11-14 14:47:47 +0000

    Add Documentation to Functions

--- luminosity_functions.py
+++ luminosity_functions.py
@@ -8,6 +8,19 @@ Created on Tue Nov 14 11:43:02 2023
 import numpy as np
 
 def GetLuminosityOfGrid(grid):
+    """ 
+    Calculates the luminosity of a grid object by iterating through all triangles and summing the areas
+    
+    Parameters
+    ----------
+    grid : matplotlib.tri.Triangulation
+        The grid containing all triangles that make up the mesh
+    
+    Returns
+    -------
+    out : float
+        Luminosity of the entire grid
+    """
     triangles = grid.triangles
     xVals, yVals = grid.x, grid.y
     
@@ -16,6 +29,10 @@ def GetLuminosityOfGrid(grid):
         x1, y1 = xVals[triangle[0]], yVals[triangle[0]]
         x2, y2 = xVals[triangle[1]], yVals[triangle[1]]
         x3, y3 = xVals[triangle[2]], yVals[triangle[2]]
+        # Calculate the area using half the determinant method:
+        #             | x1 y1 1 |
+        # Area = .5 * | x2 y2 1 |
+        #             | x3 y3 1 |
         area = 0.5 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))
         totalArea += area
     return totalArea
--- star.py
+++ star.py
@@ -9,16 +9,28 @@ import numpy as np
 from node import Node
 
 class Star:
-    def __init__(self, thePos, theRadius, theResolution):
-        self.position = thePos
-        self.radius = theRadius
+    def __init__(self, pos, radius, res):
+        """ 
+        Star class constructor
         
-        self.samples = int((4 * np.pi * self.radius**2) * theResolution / 100)
+        Parameters
+        ----------
+        pos : 3x1 array of floats
+            Position of the star in cartesian coordinates: x, y, z
+        radius : float
+            Radius of the star
+        res : float
+            Resolution, determines the number of nodes that makes up the star
+        """
+        self.position = pos
+        self.radius = radius
+        
+        samples = int((4 * np.pi * self.radius**2) * res / 100)
         
         nodes = []
         goldenRatio = np.pi * (np.sqrt(5) - 1) # Golden Ratio in radians
-        for i in range(self.samples):
-            y = 1 - (i / float(self.samples - 1)) * 2  # y goes from 1 to -1
+        for i in range(samples):
+            y = 1 - (i / float(samples - 1)) * 2  # y goes from 1 to -1
             radius = np.sqrt(1 - y * y)  # radius at y
     
             theta = goldenRatio * i  # golden angle increment
@@ -30,12 +42,32 @@ class Star:
         self.nodes = np.array(nodes)
     
     def GetAllNodes(self):
+        """ 
+        Collects all nodes that describe the star's surface
+        
+        Returns
+        -------
+        out : array of nodes
+            Array consisting of all nodes that make up the star. Nodes are with respect to the star's frame.
+        """
         allNodes = []
         for node in self.nodes:
             allNodes.append(np.array([node.x, node.y, node.z]))
         return allNodes
     
     def GetAllCoords(self):
+        """ 
+        Organises all coordinates of nodes into separate arrays for each cartesian axes
+        
+        Returns
+        -------
+        xArray : array
+            Array of all x-coordinates in the star's frame
+        yArray : array
+            Array of all y-coordinates in the star's frame
+        zArray : array
+            Array of all z-coordinates in the star's frame
+        """
         xList, yList, zList = [], [], []
         for node in self.nodes:
             xList.append(node.x)
